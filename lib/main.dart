efa2cd6e223163d596276abaea6862cdff73280a
Klayton Massango <kmassango@transcom.co.mz>
2025-05-29 20:38:53 +0200

    Firebase connect

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:safedrive/firebase_options.dart';
 import 'screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   return runApp(const SafeDriveApp());
 }
 
